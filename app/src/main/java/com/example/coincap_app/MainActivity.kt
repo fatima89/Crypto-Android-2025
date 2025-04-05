2e4383e566c0f1a952bb54fb450de7c5856ec261
fatima89 <flopezs.89@gmail.com>
2025-04-05 14:28:19 -0500

    Trabajo realizado el sabado 5

--- app/src/main/java/com/example/coincap_app/MainActivity.kt
+++ app/src/main/java/com/example/coincap_app/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.coincap_app.models.Asset
 import com.example.coincap_app.ui.theme.CoinCapAppTheme
+import com.example.coincap_app.views.AssetRow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +27,37 @@ class MainActivity : ComponentActivity() {
         setContent {
             CoinCapAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        AssetRow(
+                            Asset(
+                                id="1",
+                                name ="BTC",
+                                symbol="Bitcoin",
+                                price="87800",
+                                percentage=5.38
+
+
+                            )
+                        )
+                        Spacer(modifier = Modifier.size(16.dp))
+                        AssetRow(
+                            Asset(
+                                id="2",
+                                name ="ETH",
+                                symbol="Ethereum",
+                                price="1800",
+                                percentage=-8.28
+
+                            )
+                        )
+
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello!! $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CoinCapAppTheme {
-        Greeting("Android")
-    }
-}
