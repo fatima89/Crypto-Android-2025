b2765c1de09a6b13f2829b3b4829f9ba51adc016
fatima89 <flopezs.89@gmail.com>
2025-04-12 00:28:11 -0500

    Update CoinCapApiService.kt
    hide url

--- app/src/main/java/com/example/coincap_app/services/CoinCapApiService.kt
+++ app/src/main/java/com/example/coincap_app/services/CoinCapApiService.kt
@@ -13,7 +13,7 @@ class CoinCapApiService @Inject constructor(
 
 ) {
     suspend fun  getAssets():AssetsResponse{
-        val response: HttpResponse = client.get(urlString = "https://rest.coincap.io/v3/assets?apiKey=5cfd9d1b42ff449053e5105cb44c7c872f472b104eb2c4d72098bf309665e2fa")
+        val response: HttpResponse = client.get(urlString = "")
         return response.body()
     }
 }
